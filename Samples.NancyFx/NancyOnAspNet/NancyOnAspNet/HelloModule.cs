9eebdabd7fb0e76d5260d5c71e0ca64b33f856a2
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-01-28 01:20:38 +0300

    .

---
+++ Samples.NancyFx/NancyOnAspNet/NancyOnAspNet/HelloModule.cs
@@ -0,0 +1,12 @@
+﻿namespace NancyOnAspNet
+{
+    using Nancy;
+
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = _ => "Hello Nancy World";
+        }
+    }
+}
---
+++ Samples.NancyFx/TodoNancy/TodoNancyTests/HomeModuleTests.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoNancyTests
+{
+    using Nancy;
+    using Nancy.Testing;
+    using Xunit;
+
+    public class HomeModuleTests
+    {
+        [Fact]
+        public void ShouldAnswer200OnRootPath()
+        {
+            var sut = new Browser(new DefaultNancyBootstrapper());
+            var actual = sut.Get("/");
+            Assert.Equal(HttpStatusCode.OK, actual.StatusCode);
+        }
+    }
+}

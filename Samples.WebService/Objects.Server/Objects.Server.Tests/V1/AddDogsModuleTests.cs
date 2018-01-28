0ddbc0b34d2e20b197695139cecd6c91e982cb4f
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-01-28 03:54:16 +0300

    .

---
+++ Samples.WebService/Objects.Server/Objects.Server.Tests/V1/AddDogsModuleTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Objects.Server.Tests.V1
+{
+    using Nancy;
+    using Nancy.Testing;
+    using Xunit;
+
+    public class AddDogsModuleTests
+    {
+        private Browser sut;
+
+        public AddDogsModuleTests()
+        {
+            sut = new Browser(new DefaultNancyBootstrapper());
+        }
+
+        [Fact]
+        public void ShouldCreateDog()
+        {
+            sut.Post("/v1/dogs");
+        }
+    }
+}

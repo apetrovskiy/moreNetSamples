47c1da65717e19c64f66e36f0e7451a71b26c2dd
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-01-30 17:45:18 +0300

    .

---
+++ coding/SOLID/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple.Runner/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SingleResponsibilityPrinciple.Runner
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using ObjectModel;
+	using Types;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var reader = new DataReader();
+			var resultCollection = new List<TradeItem>();
+			Console.WriteLine(System.Environment.CurrentDirectory);
+			
+			resultCollection.AddRange(reader.LoadDataFromJsonFile(Path.Combine(Environment.CurrentDirectory, @"Resources\data001.json")));
+			resultCollection.AddRange(reader.ProcessXmlWithTrades(Path.Combine(Environment.CurrentDirectory, "Resources"), "data002.xml"));
+			resultCollection.AddRange(reader.LoadTradesFromCommaSeparatedFile(Path.Combine(Environment.CurrentDirectory, @"Resources\data003.csv")));
+			resultCollection.AddRange(reader.LoadTradesFromTsv(Path.Combine(Environment.CurrentDirectory, @"Resources\data004.tsv")));
+
+			Console.WriteLine(resultCollection.Count);
+			Console.ReadKey();
+		}
+	}
+}

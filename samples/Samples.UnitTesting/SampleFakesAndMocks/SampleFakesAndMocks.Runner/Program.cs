d7e4b56536ba661c0f524acb0ef5dbba563136be
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-02-08 17:07:34 +0300

    .

--- samples/Samples.UnitTesting/SampleFakesAndMocks/SampleFakesAndMocks.Runner/Program.cs
+++ samples/Samples.UnitTesting/SampleFakesAndMocks/SampleFakesAndMocks.Runner/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SampleFakesAndMocks.Runner
+﻿namespace SampleFakesAndMocks.Runner
 {
+	using System;
+	using System.Linq;
+
 	class Program
 	{
 		static void Main(string[] args)

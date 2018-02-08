e00adbc560d1ab4482643477b909d58c0b2abe3d
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-02-08 20:50:45 +0300

    .

--- samples/Samples.Patterns/Samples.DecoratorPattern/Samples.DecoratorPattern/Class1.cs
+++ samples/Samples.Patterns/Samples.DecoratorPattern/Samples.DecoratorPattern/Class1.cs
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-
 namespace Samples.DecoratorPattern
 {
 	public class Class1
@@ -10,15 +7,4 @@ namespace Samples.DecoratorPattern
 			// 
 		}
 	}
-=======
-﻿namespace Samples.DecoratorPattern
-{
-    public class Class1
-    {
-        public void Run()
-        {
-            // 
-        }
-    }
->>>>>>> c8d8a3a9003236eab93f3ef6bd6da7d117ff1e10
 }

d9f12c0fa2a8d1aebef11cb26c2e4791f7a341f3
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-02-22 15:22:45 +0300

    .

---
+++ samples/Samples.WebScraping/WebScrapingSample/WebScrapingSample.Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WebScrapingSample.Runner
+{
+	using System;
+	using System.Linq;
+	using HtmlAgilityPack;
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+	        var nodesLoader = new PageLoader();
+			nodesLoader.GetHtmlNodes("http://yandex.ru")
+				.Where(node => node.NodeType == HtmlNodeType.Element)
+				.ToList()
+				.ForEach(node => Console.WriteLine($"{node}: id={node.Id}, name={node.Name}, type={node.NodeType}, XPath={node.XPath}"));
+
+	        Console.ReadKey();
+        }
+    }
+}

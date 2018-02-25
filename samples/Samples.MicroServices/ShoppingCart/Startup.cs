b17737d2d9d09695f11f489ea3f0b21b8907f0e4
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-02-26 00:15:28 +0300

    .

--- samples/Samples.MicroServices/ShoppingCart/Startup.cs
+++ samples/Samples.MicroServices/ShoppingCart/Startup.cs
@@ -2,15 +2,17 @@ namespace ShoppingCart
 {
     using Microsoft.AspNetCore.Builder;
     using Nancy.Owin;
-    using Microsoft.Extensions.DependencyInjection;
+    // using Microsoft.Extensions.DependencyInjection;
 
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /*
         public void ConfigureServices(IServiceCollection services)
         {
         }
+        */
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app) // , IHostingEnvironment env, ILoggerFactory loggerFactory)

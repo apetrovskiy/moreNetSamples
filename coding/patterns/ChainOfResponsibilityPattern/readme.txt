ce03e92cb004bc3e9aa71a6901de6f1a43171567
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-03-16 18:39:36 +0300

    the singleton pattern sample

---
+++ coding/patterns/ChainOfResponsibilityPattern/readme.txt
@@ -0,0 +1,3 @@
+1) create an abstract class called Handler
+2) reorganise reader classes into ConcreteHandlers, inherited from the Handler class
+3) decide what will be the end of the chain: nothing or one of readers
---
+++ coding/patterns/SingletonPattern/SingletonPattern.Runner/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SingletonPattern.Runner
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var securitysettingsConsumer = new SecuritySettingsConsumer();
+            securitysettingsConsumer.UseSecuritySettings();
+            var numericalSettingsConsumer = new TechnicalSettingsConsumer();
+            numericalSettingsConsumer.UseNumericalSettings();
+
+            Console.ReadKey();
+        }
+    }
+}

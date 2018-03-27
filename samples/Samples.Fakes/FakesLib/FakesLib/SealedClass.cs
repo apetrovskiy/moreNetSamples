26d124dd20b805e805c7ee029854b880075e3c7e
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-03-27 08:18:17 +0300

    A Microsoft.Fakes sample

---
+++ samples/Samples.Fakes/FakesLib/FakesLib/SealedClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FakesLib
+{
+    public sealed class SealedClass
+    {
+        public string StringData { get; set; }
+        public static int IntData { get; set; }
+
+        public IEnumerable<int> GetNumbers(int count)
+        {
+            var result = new List<int>();
+            for (var i = 0; i < count; i++)
+                result.Add(i);
+            return result;
+        }
+    }
+}

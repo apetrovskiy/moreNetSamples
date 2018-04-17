f411b901868e30e85aae839af6a2ee3701dba47f
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-04-17 18:49:38 +0300

    .

---
+++ samples/Samples.MongoDb/MongoDbAccessSample/MongoDbAccessSample/DataReader.cs
@@ -0,0 +1,25 @@
+﻿namespace MongoDbAccessSample
+{
+	using System.Collections.Generic;
+
+	using MongoDB.Bson;
+	using MongoDB.Driver;
+
+	public class DataReader
+	{
+		private readonly MongoDbConnectionSettings mongoDbConnectionSettings;
+		public DataReader(MongoDbConnectionSettings settings)
+		{
+			this.mongoDbConnectionSettings = settings;
+		}
+
+		public IEnumerable<BsonDocument> GetCollection()
+		{
+
+
+
+
+			return new List<BsonDocument>();
+		}
+	}
+}
